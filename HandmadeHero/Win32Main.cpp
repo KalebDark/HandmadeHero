a7d9bae4c7fcabab3b0f162a10354a244428d51e
KalebDark <steveconlan3000@hotmail.com>
2015-06-29 14:23:57 +0100

    WIP Day 004
    Cracking on, getting there, correct the mistakes that Casey makes.
    Odd disconnect between using traditional gaming loops and Win32 messaging loops....to animate, you call RedrawWindow since you have the WM_PAINT already handling the drawing of the bitmap to screen, so all you need to do is post a WM_PAINT message to the screen. This means you need to invalidate the entire client rect of the current window, and then post a WM_PAINT.

--- HandmadeHero/Win32Main.cpp
+++ HandmadeHero/Win32Main.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include "Win32Main.h"
+#include <stdint.h>
 
 #define MAX_LOADSTRING 100
 
@@ -13,10 +14,12 @@ TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
 
 bool Running = false;
 
+const int BytesPerPixel = 4;
+
 BITMAPINFO bitmapInfo;
 void* bitmapMemory;
-HBITMAP hBitmap;
-HDC bitmapDeviceContext;
+int bitmapWidth;
+int bitmapHeight;
 
 // Forward declarations of functions included in this code module:
 ATOM				MyRegisterClass(HINSTANCE hInstance);
@@ -24,28 +27,43 @@ BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
 
+void RenderGradient(int xOffset, int yOffset) {
+	int pitch = bitmapWidth * BytesPerPixel;
+	uint8_t* row = (uint8_t*)bitmapMemory;
+	for (int y = 0; y < bitmapHeight; ++y) {
+		uint32_t* pixel = (uint32_t*)row;
+		for (int x = 0; x < bitmapWidth; ++x) {
+			uint8_t blue = x + xOffset;
+			uint8_t green = y + yOffset;
+			*pixel++ = (green << 8 | blue);
+		}
+		row += pitch;
+	}
+}
+
 void Win32ResizeDIBSection(int width, int height) {
-
-	if (hBitmap) {
-		DeleteObject(hBitmap);
+	if (bitmapMemory) {
+		VirtualFree(bitmapMemory, 0, MEM_RELEASE);
 	}
 
-	if (!bitmapDeviceContext) {
-		bitmapDeviceContext = CreateCompatibleDC(0);
-	}
+	bitmapWidth = width;
+	bitmapHeight = height;
 
 	bitmapInfo.bmiHeader.biSize = sizeof(bitmapInfo.bmiHeader);
-	bitmapInfo.bmiHeader.biWidth = width;
-	bitmapInfo.bmiHeader.biHeight = height;
+	bitmapInfo.bmiHeader.biWidth = bitmapWidth;
+	bitmapInfo.bmiHeader.biHeight = bitmapHeight;
 	bitmapInfo.bmiHeader.biPlanes = 1;
 	bitmapInfo.bmiHeader.biBitCount = 32;
 	bitmapInfo.bmiHeader.biCompression = BI_RGB;
 
-	hBitmap = CreateDIBSection(bitmapDeviceContext, &bitmapInfo, DIB_RGB_COLORS, &bitmapMemory, 0, 0);
+	//hBitmap = CreateDIBSection(bitmapDeviceContext, &bitmapInfo, DIB_RGB_COLORS, &bitmapMemory, 0, 0);
+	int bitmapMemorySize = (bitmapWidth * bitmapHeight) * BytesPerPixel;
+	bitmapMemory = VirtualAlloc(0, bitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+	ZeroMemory(bitmapMemory, bitmapMemorySize);
 }
 
-void Win32UpdateWindow(HDC hDC, int x, int y, int width, int height) {
-	StretchDIBits(hDC, x, y, width, height, x, y, width, height, bitmapMemory, &bitmapInfo, DIB_RGB_COLORS, SRCCOPY);
+void Win32UpdateWindow(HDC hDC, int x, int y, int windowWidth, int windowHeight) {
+	StretchDIBits(hDC, x, y, bitmapWidth, bitmapHeight, x, y, windowWidth, windowHeight, bitmapMemory, &bitmapInfo, DIB_RGB_COLORS, SRCCOPY);
 }
 
 int APIENTRY _tWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nCmdShow) {
@@ -65,16 +83,26 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstanc
 	Running = true;
 
 	// Main message loop:
+	int xOffset = 0;
+	int yOffset = 0;
 	MSG msg;
 	while (Running) {
 		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			if (msg.message == WM_QUIT) {
+				Running = false;
+				break;
+			}
+
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
-	}
 
-	DeleteObject(hBitmap);
-	DeleteDC(bitmapDeviceContext);
+		RenderGradient(xOffset, yOffset);
+		xOffset += 1;
+		yOffset += 1;
+
+		RedrawWindow(msg.hwnd, 0, 0, RDW_INVALIDATE);
+	}
 
 	return (int)msg.wParam;
 }
@@ -139,7 +167,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 
 			Win32UpdateWindow(hdc, x, y, width, height);
 
-			PatBlt(hdc, ps.rcPaint.left, ps.rcPaint.top, ps.rcPaint.right - ps.rcPaint.left, ps.rcPaint.bottom - ps.rcPaint.top, 0);
+			//PatBlt(hdc, ps.rcPaint.left, ps.rcPaint.top, ps.rcPaint.right - ps.rcPaint.left, ps.rcPaint.bottom - ps.rcPaint.top, 0);
 
 			EndPaint(hWnd, &ps);
 			break;
